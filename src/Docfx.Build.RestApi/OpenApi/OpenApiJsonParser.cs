379b0c25154161314f467cfb187bf0906be202a9
Doug Clutter <ClutterD@michigan.gov>
2024-01-26 11:00:19 -0500

    Updated so only a Verbose message is logged for files that are not OpenApi 3.0.  This was necessary because ALL JSON files are being handled by this parser and other unit tests rely on a specific error count.

--- src/Docfx.Build.RestApi/OpenApi/OpenApiJsonParser.cs
+++ src/Docfx.Build.RestApi/OpenApi/OpenApiJsonParser.cs
@@ -11,23 +11,24 @@ public class OpenApiJsonParser
         using var fileStream = File.OpenRead(fullPath);
         var reader = new OpenApiStreamReader(new() { ReferenceResolution = ReferenceResolutionSetting.ResolveLocalReferences });
         var document = reader.Read(fileStream, out OpenApiDiagnostic diagnostic);
-        if (diagnostic.Errors.Any())
-        {
-            foreach (var error in diagnostic.Warnings)
-            {
-                Logger.LogWarning($"OpenApi parse warnings: {error.Message}", file: fullPath);
-            }
-            foreach (var error in diagnostic.Errors )
-            {
-                Logger.LogError($"OpenApi parse errors: {error.Message}", file: fullPath);
-            }
-            return null;
-        }
         if (diagnostic?.SpecificationVersion == OpenApiSpecVersion.OpenApi3_0)
         {
-            return new() { Document = document };
+            LogDiagnostics(fullPath, diagnostic);
+            return diagnostic.Errors.Count > 0 ? null : new() { Document = document };
         }
-        Logger.LogWarning($"OpenApi version not supported: {diagnostic?.SpecificationVersion}");
+        Logger.LogVerbose($"OpenApi version not supported: {diagnostic?.SpecificationVersion}");
         return null;
+    }
+
+    private static void LogDiagnostics(string fullPath, OpenApiDiagnostic diagnostic)
+    {
+        foreach (var error in diagnostic.Warnings)
+        {
+            Logger.LogWarning($"OpenApi parse warnings: {error.Message}", file: fullPath);
+        }
+        foreach (var error in diagnostic.Errors)
+        {
+            Logger.LogError($"OpenApi parse errors: {error.Message}", file: fullPath);
+        }
     }
 }
